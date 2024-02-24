5467870ec89d3be1da01ae5a0b734aa6f3cfca28
VEDANT <vedantmakwana3@gmail.com>
2024-02-24 11:08:28 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Local Notifications Demo',
+    home: Notify(),
+  ));
+}
+
+class Notify extends StatefulWidget {
+  const Notify({Key? key}) : super(key: key);
+
+  @override
+  State<Notify> createState() => _NotifyState();
+}
+
+class _NotifyState extends State<Notify> {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+  Future<void> _showNotification() async {
+    var initializationSettingsAndroid =
+    const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = const IOSInitializationSettings();
+    var initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: (String? payload) async {},
+    );
+
+    var androidDetails = const AndroidNotificationDetails(
+      'channelId',
+      'Local Notification',
+      'This is the description of the notification',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+      // sound: RawResourceAndroidNotificationSound('notification'),
+    );
+    var iosDetails = const IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidDetails,
+      iOS: iosDetails,
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Hello.Dev',
+      'Now,you are a developer',
+      platformChannelSpecifics,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("notify"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _showNotification(),
+          child: const Text('Show Notification'),
+        ),
+      ),
+    );
+  }
+}
